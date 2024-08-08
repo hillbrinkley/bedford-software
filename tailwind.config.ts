c7dfa979761c484252227d85bd12e9c0c85cb628
Hill, Brinkley Scott (IT System Verification and Testing) <bhill100@liberty.edu>
2024-08-08 13:38:33 -0400

    push

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import { colorForIntegration } from "astro/runtime/client/dev-toolbar/apps/utils/icons.js";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        'bedford-blue': '#378FDF'
+      },
       fontFamily: {
         sans: ["Plus Jakarta Sans Variable", ...defaultTheme.fontFamily.sans],
         serif: ["Inria Serif", ...defaultTheme.fontFamily.serif]
@@ -13,6 +17,7 @@ export default {
       backgroundImage: {
         'custom-image': 'url("/background.svg")',
       },
+
 
     },
   },
