b39e4f9fd0f642db7d4e736f9964ced6f9992138
Hill, Brinkley Scott (IT System Verification and Testing) <bhill100@liberty.edu>
2024-09-12 22:09:45 -0400

    does this fix the header?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
  output: "server",
- integrations: [tailwind()],
+ integrations: [tailwind(), icon()],
  adapter: netlify(),
 });
